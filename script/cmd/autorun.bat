d484a805990443dc40450fd7c92166fb0df32920
Osama Hassanein <player1os@gmail.com>
2018-09-16 19:21:14 +0200

    Modify for certificates and add windows configurations

--- script/cmd/autorun.bat
+++ script/cmd/autorun.bat
@@ -1,3 +1,2 @@
 @ECHO OFF
 SET PATH=C:\Programs\nodejs;C:\Programs\putty;C:\Programs\vscode;C:\Programs\curl\bin;.\node_modules\.bin\;%PATH%
-SET NODE_EXTRA_CA_CERTS=C:\Users\osama.hassanein\.cert\ca-bundle.crt
--- script/pm2/example_application.js
+++
@@ -1,3 +0,0 @@
-if (process.send !== undefined) {
-	process.send('ready')
-}
