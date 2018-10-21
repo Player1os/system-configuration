533d82a64ad05a81b131086e80a2786523242ee7
Osama Hassanein <player1os@gmail.com>
2018-10-21 09:43:52 +0200

    Add autoaccepting to the update system script

--- script/linux/update_system.sh
+++ script/linux/update_system.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get upgrade
+sudo apt-get dist-upgrade -y
+sudo apt-get upgrade -y
 sudo apt-get purge
 sudo apt-get autoremove
 sudo apt-get autoclean
