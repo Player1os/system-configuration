a59c3109d5a4f75302d08647afb6315f04b37cf4
Osama Hassanein <player1os@gmail.com>
2018-10-23 08:42:11 +0200

    Add -y to the update script commands

--- script/linux/update_system.sh
+++ script/linux/update_system.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get upgrade -y
-sudo apt-get purge
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo apt-get clean
+sudo apt-get purge -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+sudo apt-get clean -y
