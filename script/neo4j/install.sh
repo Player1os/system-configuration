8f5a6440db55586eb399febc09c67ded0302ceb0
Osama Hassanein <osama.e.hassanein@gmail.com>
2019-04-24 16:46:28 +0200

    Add a prototype for the neo4j install script

---
+++ script/neo4j/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+## https://askubuntu.com/questions/1044449/install-neo4j-ubuntu-18-04
+## https://neo4j.com/docs/operations-manual/current/configuration/set-initial-password/ (it didn’t actually work)
+## PUTTY Forward ports 7473, 7474, 7687 https://neo4j.com/developer/kb/how-to-setup-neo4j-to-startup-on-linux-server-reboot/
