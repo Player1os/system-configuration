00a47d2438220341faff89490c68bf8ef9895bd2
Osama Hassanein <osama.e.hassanein@gmail.com>
2019-04-25 00:04:23 +0200

    Add build dependencies to the setup.sh script

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ chmod +x ~/upgrade_release.sh
 ~/upgrade_release.sh
 
 # Install the build dependency packages.
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python-openssl git
 
 # Install Python through pyenv.
 . ./script/python/pyenv.sh
