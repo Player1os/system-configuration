cc5fc61b891bb2d6d3651f8843768309fee9ef53
Osama Hassanein <osama.e.hassanein@gmail.com>
2019-04-25 03:25:38 +0200

    Add the neo4j proper install script

--- script/neo4j/install.sh
+++ script/neo4j/install.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
-## https://askubuntu.com/questions/1044449/install-neo4j-ubuntu-18-04
-## https://neo4j.com/docs/operations-manual/current/configuration/set-initial-password/ (it didn’t actually work)
-## PUTTY Forward ports 7473, 7474, 7687 https://neo4j.com/developer/kb/how-to-setup-neo4j-to-startup-on-linux-server-reboot/
+# Add the repository.
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb https://debian.neo4j.org/repo stable/' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
+
+# Install the server.
+sudo apt-get install -y neo4j
+
+# Start the server.
+sudo service neo4j start
+
+# Output the default username, password and web GUI.
+echo "The default username and password is 'neo4j'"
+echo "The web GUI is available at http://localhost:7474/"
+
+# Configure the server to start on system boot.
+sudo cp ./script/neo4j/neo4j_ctl /etc/init.d/neo4j_ctl
+sudo chmod 744 /etc/init.d/neo4j_ctl
+
+if [[ $(/sbin/runlevel) == "N 3" ]]; then
+	cd /etc/rc3.d
+	sudo ln -s ../init.d/neo4j_ctl S40neo4j_ctl
+	cd /etc/rc0.d
+	sudo ln -s ../init.d/neo4j_ctl K30neo4j_ctl
+fi
+
+if [[ $(/sbin/runlevel) == "N 5" ]]; then
+	cd /etc/rc5.d
+	sudo ln -s ../init.d/neo4j_ctl S40neo4j_ctl
+	cd /etc/rc0.d
+	sudo ln -s ../init.d/neo4j_ctl K30neo4j_ctl
+fi
