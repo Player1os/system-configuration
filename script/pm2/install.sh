413da8a38d3292efb1445fad8474d0377052d350
Osama Hassanein <osama.e.hassanein@gmail.com>
2019-05-08 14:25:12 +0200

    Add pm2 to update_system upon install

--- script/pm2/install.sh
+++ script/pm2/install.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 # In the global nodenv instance run.
-npm i -g pm2
+npm i -g pm2@latest
 
 # Configure for startup.
 pm2 startup
 
 # Configure the saving of the currently running instances.
 pm2 save
+
+# Add update commands to the update_system.sh script.
+echo '' >> ~/update_system.sh
+echo '# Update pm2.' >> ~/update_system.sh
+echo 'npm i -g pm2@latest' >> ~/update_system.sh
