79f1dedfd5ef12ee46481a465a7bd0cd71d5b149
Osama Hassanein <osama.e.hassanein@gmail.com>
2019-05-08 14:05:28 +0200

    Fix setup.bat and add surge and serve npm apps

---
+++ setup.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:: Install the current project's dependencies.
+call npm i
+
+:: Install the drawio local instance.
+call npm start script/drawio/install
+
+:: Load the git configurations files.
+call npm start script/git
+
+:: Load the nodejs configuration files.
+call npm start script/nodejs
+
+:: Load the vscode configurations files.
+call npm start script/vscode
+
+:: Install the serve npm application.
+call npm i -g serve@latest
+
+:: Install the surge npm application.
+call npm i -g surge@latest
